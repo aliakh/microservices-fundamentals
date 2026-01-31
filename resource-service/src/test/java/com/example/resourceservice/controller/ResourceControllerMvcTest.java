10b00796988877a9f220dad6f7cf52181ad93a6b
Aliaksandr_Liakh <Aliaksandr_Liakh1@epam.com>
2026-01-31 14:49:07 +0300

    Implement practical task for module 3

--- resource-service/src/test/java/com/example/resourceservice/controller/ResourceControllerMvcTest.java
+++ resource-service/src/test/java/com/example/resourceservice/controller/ResourceControllerMvcTest.java
@@ -1,5 +1,8 @@
 package com.example.resourceservice.controller;
 
+import com.example.resourceservice.service.ResourceService;
+import com.example.resourceservice.service.SongServiceClient;
+import com.example.resourceservice.service.validation.IdValidator;
 import com.jayway.jsonpath.JsonPath;
 import com.example.resourceservice.AbstractIntegrationTest;
 import org.junit.jupiter.api.Test;
@@ -10,10 +13,15 @@ import org.springframework.core.io.ClassPathResource;
 import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.util.ReflectionTestUtils;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.transaction.annotation.Transactional;
 
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -34,22 +42,20 @@ public class ResourceControllerMvcTest extends AbstractIntegrationTest {
 
     @Autowired
     private MockMvc mockMvc;
+    @MockitoBean
+    private SongServiceClient songServiceClient;
 
-//    @Test
-//    @Transactional
-//    void shouldUploadResource() throws Exception {
-//        var content = new ClassPathResource(FILE_PATH).getInputStream().readAllBytes();
-//        var multipartFile = new MockMultipartFile(
-//            "file",
-//            FILE_NAME,
-//            CONTENT_TYPE_AUDIO_MPEG,
-//            content
-//        );
-//
-//        mockMvc.perform(MockMvcRequestBuilders.multipart(URL_PATH).file(multipartFile))
-//            .andExpect(status().isOk())
-//            .andExpect(jsonPath(".id").isNotEmpty());
-//    }
+    @Test
+    @Transactional
+    void shouldUploadResource() throws Exception {
+        var audio = new ClassPathResource(FILE_PATH).getInputStream().readAllBytes();
+
+        mockMvc.perform(MockMvcRequestBuilders.post(URL_PATH)
+                .content(audio)
+                .contentType("audio/mpeg"))
+            .andExpect(status().isOk())
+            .andExpect(jsonPath(".id").isNotEmpty());
+    }
 //
     @Test
     @Transactional
@@ -103,25 +109,35 @@ public class ResourceControllerMvcTest extends AbstractIntegrationTest {
 //            .andExpect(content().contentType(CONTENT_TYPE_AUDIO_MPEG))
 //            .andExpect(content().bytes(content));
 //    }
-//
-//    @Test
-//    @Transactional
-//    void shouldDeleteResource() throws Exception {
-//        var content = new ClassPathResource(FILE_PATH).getInputStream().readAllBytes();
+
+    @Test
+    @Transactional
+    void shouldDeleteResource() throws Exception {
+        var audio = new ClassPathResource(FILE_PATH).getInputStream().readAllBytes();
 //        var multipartFile = new MockMultipartFile(
 //            "file",
 //            FILE_NAME,
 //            CONTENT_TYPE_AUDIO_MPEG,
 //            content
 //        );
-//
-//        var uploadResult = mockMvc.perform(MockMvcRequestBuilders.multipart(URL_PATH).file(multipartFile))
-//            .andReturn();
-//        var uploadResponseContent = uploadResult.getResponse().getContentAsString();
-//        var id = JsonPath.read(uploadResponseContent, "$.id");
-//
-//        mockMvc.perform(MockMvcRequestBuilders.delete(URL_PATH).param("ids", id.toString()))
-//            .andExpect(status().isOk())
-//            .andExpect(jsonPath(".ids[0]").value(id));
-//    }
+
+        var uploadResult = mockMvc.perform(MockMvcRequestBuilders.post(URL_PATH)
+                .content(audio)
+                .contentType("audio/mpeg"))
+            .andReturn();
+        var uploadResponseContent = uploadResult.getResponse().getContentAsString();
+        var id = JsonPath.read(uploadResponseContent, "$.id");
+
+//        ReflectionTestUtils.setField(resourceService, "idValidator", new IdValidator());
+        doNothing().when(songServiceClient).deleteSong(Long.valueOf(id.toString()));
+
+        mockMvc.perform(MockMvcRequestBuilders.delete(URL_PATH).param("id", id.toString()))
+            .andExpect(status().isOk())
+            .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+            .andExpect(jsonPath("$.ids.length()").value(1))
+            .andExpect(jsonPath("$.ids[0]").value(id));
+
+        verify(songServiceClient).deleteSong(Long.valueOf(id.toString()));
+        verifyNoMoreInteractions(songServiceClient);
+    }
 }
