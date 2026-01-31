74842c63603646f4e72d083952e32ca910725786
Aliaksandr_Liakh <Aliaksandr_Liakh1@epam.com>
2026-01-31 15:55:36 +0300

    Implement practical task for module 3

--- resource-service/src/test/java/com/example/resourceservice/controller/ResourceControllerApplicationTest.java
+++ resource-service/src/test/java/com/example/resourceservice/controller/ResourceControllerApplicationTest.java
@@ -1,9 +1,12 @@
 package com.example.resourceservice.controller;
 
 import com.example.resourceservice.AbstractIntegrationTest;
+import com.example.resourceservice.dto.DeleteResourcesResponse;
+import com.example.resourceservice.dto.UploadResourceResponse;
 import com.example.resourceservice.entity.Resource;
 import com.example.resourceservice.repository.ResourceRepository;
 import com.example.resourceservice.service.S3Service;
+import com.example.resourceservice.service.SongServiceClient;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,6 +21,9 @@ import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.util.UriComponentsBuilder;
@@ -29,8 +35,18 @@ import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("test")
+//@TestPropertySource(properties = {
+//    "spring.datasource.url=jdbc:tc:postgresql:17.0://localhost:5433/resource_db",
+//    "spring.datasource.driver-class-name=org.testcontainers.jdbc.ContainerDatabaseDriver",
+//    "spring.jpa.hibernate.ddl-auto=create",
+//    "spring.cloud.discovery.enabled=false"
+//})
 public class ResourceControllerApplicationTest extends AbstractIntegrationTest {
 
     private static final String URL_PATH = "/resources";
@@ -45,36 +61,45 @@ public class ResourceControllerApplicationTest extends AbstractIntegrationTest {
     private ResourceRepository resourceRepository;
     @Autowired
     private S3Service s3Service;
+    @MockitoBean
+    private SongServiceClient songServiceClient;
 
     @BeforeEach
     void init() {
         resourceRepository.deleteAll();
     }
 
-//    @Test
-//    void shouldUploadResource() throws Exception {
-//        var content = new ClassPathResource(FILE_PATH).getInputStream().readAllBytes();
-//        var requestEntity = new HttpEntity<>(getMultipartBody(content), getMultipartHeaders());
-//
-//        var responseEntity = testRestTemplate.postForEntity(URL_PATH, requestEntity, ResourceUploadedResponse.class);
-//        assertEquals(HttpStatus.OK, responseEntity.getStatusCode());
-//        assertEquals(MediaType.APPLICATION_JSON, responseEntity.getHeaders().getContentType());
-//
-//        var resourceUploadedResponse = responseEntity.getBody();
-//        assertNotNull(resourceUploadedResponse);
-//        assertNotNull(resourceUploadedResponse.id());
-//
-//        var foundResourceEntity = resourceRepository.findById(resourceUploadedResponse.id());
-//        assertTrue(foundResourceEntity.isPresent());
-//
-//        var actualResourceEntity = foundResourceEntity.get();
-//        assertEquals(resourceUploadedResponse.id(), actualResourceEntity.getId());
+    @Test
+    void shouldUploadResource() throws Exception {
+        var content = new ClassPathResource(FILE_PATH).getInputStream().readAllBytes();
+// 2. Set headers for a binary stream
+        HttpHeaders headers = new HttpHeaders();
+        headers.add(HttpHeaders.CONTENT_TYPE, "audio/mpeg");
+//        headers.setContentType(MediaType.APPLICATION_OCTET_STREAM);
+
+// 3. Create entity with byte[] directly
+        var requestEntity = new HttpEntity<>(content, headers);
+
+// 4. Execute the POST
+        var responseEntity = testRestTemplate.postForEntity(URL_PATH, requestEntity, UploadResourceResponse.class);
+        assertEquals(HttpStatus.OK, responseEntity.getStatusCode());
+        assertEquals(MediaType.APPLICATION_JSON, responseEntity.getHeaders().getContentType());
+
+        var resourceUploadedResponse = responseEntity.getBody();
+        assertNotNull(resourceUploadedResponse);
+        assertNotNull(resourceUploadedResponse.id());
+
+        var foundResourceEntity = resourceRepository.findById(resourceUploadedResponse.id());
+        assertTrue(foundResourceEntity.isPresent());
+
+        var actualResourceEntity = foundResourceEntity.get();
+        assertEquals(resourceUploadedResponse.id(), actualResourceEntity.getId());
 //        assertEquals(BUCKET, actualResourceEntity.getBucket());
 //        assertTrue(Pattern.matches(UUID_REGEXP, actualResourceEntity.getKey()));
 //        assertEquals(FILE_NAME, actualResourceEntity.getName());
 //        assertEquals(content.length, actualResourceEntity.getSize());
-//    }
-//
+    }
+
 //    private MultiValueMap<String, Object> getMultipartBody(byte[] content) {
 //        MultiValueMap<String, String> headers = new LinkedMultiValueMap<>();
 //
@@ -100,8 +125,25 @@ public class ResourceControllerApplicationTest extends AbstractIntegrationTest {
 //        return headers;
 //    }
 //
-//    @Test
-//    void shouldDownloadResource() throws IOException {
+    @Test
+    void shouldDownloadResource() throws IOException {
+        var content = new ClassPathResource(FILE_PATH).getInputStream().readAllBytes();
+// 2. Set headers for a binary stream
+        HttpHeaders headers = new HttpHeaders();
+        headers.add(HttpHeaders.CONTENT_TYPE, "audio/mpeg");
+//        headers.setContentType(MediaType.APPLICATION_OCTET_STREAM);
+
+// 3. Create entity with byte[] directly
+        var requestEntity = new HttpEntity<>(content, headers);
+
+// 4. Execute the POST
+        var responseEntity = testRestTemplate.postForEntity(URL_PATH, requestEntity, UploadResourceResponse.class);
+        assertEquals(HttpStatus.OK, responseEntity.getStatusCode());
+        assertEquals(MediaType.APPLICATION_JSON, responseEntity.getHeaders().getContentType());
+
+        var resourceUploadedResponse = responseEntity.getBody();
+        assertNotNull(resourceUploadedResponse);
+        assertNotNull(resourceUploadedResponse.id());
 //        var content = new ClassPathResource(FILE_PATH).getInputStream().readAllBytes();
 //        var multipartFile = new MockMultipartFile(
 //            "file",
@@ -120,46 +162,72 @@ public class ResourceControllerApplicationTest extends AbstractIntegrationTest {
 //        resourceEntity.setName(FILE_NAME);
 //        resourceEntity.setSize((long) content.length);
 //        var savedResourceEntity = resourceRepository.save(resourceEntity);
-//
-//        var responseEntity = testRestTemplate.getForEntity(URL_PATH + "/" + savedResourceEntity.getId() + "/download", ByteArrayResource.class);
-//        assertEquals(HttpStatus.OK, responseEntity.getStatusCode());
-//        assertEquals(CONTENT_TYPE_AUDIO_MPEG, responseEntity.getHeaders().getContentType().toString());
-//
-//        assertNotNull(responseEntity.getBody());
-//        assertArrayEquals(content, responseEntity.getBody().getByteArray());
-//    }
-//
-//    @Test
-//    void shouldDeleteResource() throws IOException {
+
+        var responseEntity2 = testRestTemplate.getForEntity(URL_PATH + "/" + resourceUploadedResponse.id(), byte[].class);
+        assertEquals(HttpStatus.OK, responseEntity2.getStatusCode());
+        assertEquals("audio/mpeg", responseEntity2.getHeaders().getContentType().toString());
+
+        assertNotNull(responseEntity2.getBody());
+        assertArrayEquals(content, responseEntity2.getBody());
+    }
+
+    @Test
+    void shouldDeleteResource() throws IOException {
 //        var content = new ClassPathResource(FILE_PATH).getInputStream().readAllBytes();
 //
 //        var resourceEntity = new Resource();
-//        resourceEntity.setBucket(BUCKET);
+////        resourceEntity.setBucket(BUCKET);
 //        resourceEntity.setKey(KEY);
-//        resourceEntity.setName(FILE_NAME);
-//        resourceEntity.setSize((long) content.length);
+////        resourceEntity.setName(FILE_NAME);
+////        resourceEntity.setSize((long) content.length);
 //
 //        var savedResourceEntity = resourceRepository.save(resourceEntity);
-//
-//        var headers = new HttpHeaders();
-//        headers.set(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE);
-//
-//        var responseEntity = testRestTemplate.exchange(
-//            UriComponentsBuilder.fromUriString(URL_PATH).queryParam("ids", savedResourceEntity.getId()).build().toUri(),
-//            HttpMethod.DELETE,
-//            new HttpEntity<>(headers),
-//            ResourcesDeletedResponse.class
-//        );
-//        assertEquals(HttpStatus.OK, responseEntity.getStatusCode());
-//        assertEquals(MediaType.APPLICATION_JSON, responseEntity.getHeaders().getContentType());
-//
-//        var resourceDeletedResponse = responseEntity.getBody();
-//        assertNotNull(resourceDeletedResponse);
-//        assertNotNull(resourceDeletedResponse.ids());
-//        assertEquals(1, resourceDeletedResponse.ids().size());
-//
-//        var foundResourceEntities = resourceRepository.findAllById(resourceDeletedResponse.ids());
-//        assertNotNull(foundResourceEntities);
-//        assertTrue(foundResourceEntities.isEmpty());
-//    }
+
+
+        var content = new ClassPathResource(FILE_PATH).getInputStream().readAllBytes();
+// 2. Set headers for a binary stream
+        HttpHeaders headers = new HttpHeaders();
+        headers.add(HttpHeaders.CONTENT_TYPE, "audio/mpeg");
+//        headers.setContentType(MediaType.APPLICATION_OCTET_STREAM);
+
+// 3. Create entity with byte[] directly
+        var requestEntity = new HttpEntity<>(content, headers);
+
+// 4. Execute the POST
+        var responseEntity = testRestTemplate.postForEntity(URL_PATH, requestEntity, UploadResourceResponse.class);
+        assertEquals(HttpStatus.OK, responseEntity.getStatusCode());
+        assertEquals(MediaType.APPLICATION_JSON, responseEntity.getHeaders().getContentType());
+
+        var resourceUploadedResponse = responseEntity.getBody();
+        assertNotNull(resourceUploadedResponse);
+        assertNotNull(resourceUploadedResponse.id());
+
+//        var foundResourceEntity = resourceRepository.findById(resourceUploadedResponse.id());
+
+        doNothing().when(songServiceClient).deleteSong(resourceUploadedResponse.id());
+
+        var headers2 = new HttpHeaders();
+        headers.set(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE);
+
+        var responseEntity2 = testRestTemplate.exchange(
+            UriComponentsBuilder.fromUriString(URL_PATH).queryParam("id", resourceUploadedResponse.id()).build().toUri(),
+            HttpMethod.DELETE,
+            new HttpEntity<>(headers2),
+            DeleteResourcesResponse.class
+        );
+        assertEquals(HttpStatus.OK, responseEntity2.getStatusCode());
+        assertEquals(MediaType.APPLICATION_JSON, responseEntity2.getHeaders().getContentType());
+
+        var resourceDeletedResponse = responseEntity2.getBody();
+        assertNotNull(resourceDeletedResponse);
+        assertNotNull(resourceDeletedResponse.ids());
+        assertEquals(1, resourceDeletedResponse.ids().size());
+
+        verify(songServiceClient).deleteSong(resourceUploadedResponse.id());
+        verifyNoMoreInteractions(songServiceClient);
+
+        var foundResourceEntities = resourceRepository.findAllById(resourceDeletedResponse.ids());
+        assertNotNull(foundResourceEntities);
+        assertTrue(foundResourceEntities.isEmpty());
+    }
 }
