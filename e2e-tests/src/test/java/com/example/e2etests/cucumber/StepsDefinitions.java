f8c917fef2113a06c22d2882a309e5a23db2b3a5
Aliaksandr_Liakh <Aliaksandr_Liakh1@epam.com>
2026-02-03 21:48:18 +0300

    Fix

--- e2e-tests/src/test/java/com/example/e2etests/cucumber/StepsDefinitions.java
+++ e2e-tests/src/test/java/com/example/e2etests/cucumber/StepsDefinitions.java
@@ -1,5 +1,8 @@
 package com.example.e2etests.cucumber;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import org.springframework.core.io.ClassPathResource;
@@ -15,13 +18,20 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 public class StepsDefinitions {
 
-    private static final String FILE_PATH = "/audio/Kevin MacLeod - Impact Moderato.mp3";
+    private static final String FILE_PATH = "/audio/audio2.mp3";
+
+    private final ObjectMapper objectMapper;
+
+    public StepsDefinitions(ObjectMapper objectMapper) {
+        this.objectMapper = objectMapper;
+    }
 
     private final RestTemplate restTemplate = new RestTemplate();
 
     private Integer postResourceId;
 
-    @When("upload file with name {string} to the resource service")
+
+    @When("upload the audio file {string} to the resource service")
     public void upload_file_to_the_resource_service(String fileName) throws IOException {
 //        HttpEntity<MultiValueMap<String, Object>> entity = getMultipartEntity(fileName);
         String RESOURCES_URL = "http://localhost:8083/resources";
@@ -44,7 +54,7 @@ public class StepsDefinitions {
         assertNotNull(uploadedId);
     }
 
-    @Then("wait for processor service to parse data")
+    @Then("wait for the resource processor to consume the resource")
     public void wait_for_processor_service_to_parse_data() throws InterruptedException {
 //        TimeUnit.SECONDS.sleep(60);
 
@@ -64,17 +74,26 @@ public class StepsDefinitions {
         }
     }
 
-    @Then("check data is saved via GET call to the song service")
-    public void check_data_is_saved_via_get_call_to_the_song_service() {
+    @Then("I get the song metadata from the song service")
+    public void check_data_is_saved_via_get_call_to_the_song_service(String json) throws JsonProcessingException {
         String url = "http://localhost:8084/songs/" + postResourceId;
         ResponseEntity<Map> response = restTemplate.exchange(url, HttpMethod.GET, null, Map.class);
         int statusCodeValue = response.getStatusCodeValue();
+
+        var expectedSongDto = objectMapper.readValue(json, new TypeReference<Map>() {
+        });
+//        var actualSongDto = response.getBody();
+
         var actual = response.getBody();
+
+        expectedSongDto.forEach((key,value) -> {
+            assertEquals(value, actual.get(key));
+        });
 //        var expected = getSongDto();
 
-        assertEquals(200, statusCodeValue);
-        assertNotNull(actual);
-        assertEquals("Impact Moderato", actual.get("name"));
+//        assertEquals(200, statusCodeValue);
+//        assertNotNull(actual);
+//        assertEquals("Impact Moderato", actual.get("name"));
 //        assertEquals(expected.getArtist(), actual.getArtist());
 //        assertEquals(expected.getAlbum(), actual.getAlbum());
 //        assertEquals(expected.getLength(), actual.getLength());
--- e2e-tests/src/test/resources/features/e2e.feature
+++ e2e-tests/src/test/resources/features/e2e.feature
@@ -1,6 +1,15 @@
 Feature: check upload services possibilities
 
   Scenario: file uploaded, metadata parsed and persisted
-    When upload file with name 'Test.mp3' to the resource service
-    Then wait for processor service to parse data
-    And check data is saved via GET call to the song service
+    When upload the audio file '/audio/audio2.mp3' to the resource service
+    Then wait for the resource processor to consume the resource
+    And I get the song metadata from the song service
+      """
+      {
+        "name": "Impact Moderato",
+        "artist": "Kevin MacLeod",
+        "album": "Impact",
+        "duration": "01:16",
+        "year": "2014"
+      }
+      """
