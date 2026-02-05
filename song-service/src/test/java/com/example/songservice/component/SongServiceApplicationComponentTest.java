ba52f41d1be89e6972caf5dab9026f3ceb9775a7
Aliaksandr_Liakh <Aliaksandr_Liakh1@epam.com>
2026-02-05 10:09:52 +0300

    cuc 4

--- song-service/src/test/java/com/example/songservice/component/SongServiceApplicationComponentTest.java
+++ song-service/src/test/java/com/example/songservice/component/SongServiceApplicationComponentTest.java
@@ -1,7 +1,12 @@
 package com.example.songservice.component;
 
-import org.junit.platform.suite.api.*;
-import static io.cucumber.junit.platform.engine.Constants.*;
+import org.junit.platform.suite.api.ConfigurationParameter;
+import org.junit.platform.suite.api.IncludeEngines;
+import org.junit.platform.suite.api.SelectClasspathResource;
+import org.junit.platform.suite.api.Suite;
+
+import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
+import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME;
 
 @Suite
 @IncludeEngines("cucumber")
