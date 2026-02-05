b9e0ee8b9dbe1b05b6de350f41a4011a7d4cfd82
Aliaksandr_Liakh <Aliaksandr_Liakh1@epam.com>
2026-02-05 10:44:12 +0300

    Implement practical task for module 3

--- resource-service/src/test/java/com/example/resourceservice/component/ResourceServiceApplicationComponentTest.java
+++ resource-service/src/test/java/com/example/resourceservice/component/ResourceServiceApplicationComponentTest.java
@@ -1,17 +1,17 @@
 package com.example.resourceservice.component;
 
-import com.example.resourceservice.AbstractTestcontainersTest;
-import io.cucumber.junit.Cucumber;
-import io.cucumber.junit.CucumberOptions;
-import io.cucumber.spring.CucumberContextConfiguration;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
+import org.junit.platform.suite.api.ConfigurationParameter;
+import org.junit.platform.suite.api.IncludeEngines;
+import org.junit.platform.suite.api.SelectClasspathResource;
+import org.junit.platform.suite.api.Suite;
 
-@RunWith(Cucumber.class)
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-@ActiveProfiles("test")
-@CucumberOptions(features = "src/test/resources/features", plugin = {"pretty", "html:target/test-results/test/cucumber.html"})
-@CucumberContextConfiguration
-public class ResourceServiceApplicationComponentTest extends AbstractTestcontainersTest {
+import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
+import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME;
+
+@Suite
+@IncludeEngines("cucumber")
+@SelectClasspathResource("features")
+@ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "com.example.resourceservice.component")
+@ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "pretty, html:target/test-results/test/cucumber.html")
+public class ResourceServiceApplicationComponentTest {
 }
