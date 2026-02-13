ec13d5a6bfaaf586ba1cdf9fe1cb407c213dd246
Aliaksandr Liakh <Aliaksandr_Liakh1@epam.com>
2026-02-13 14:36:02 +0300

    Implement practical task for module 5

--- resource-processor/src/main/java/com/example/resourceprocessor/service/ResourceServiceClient.java
+++ resource-processor/src/main/java/com/example/resourceprocessor/service/ResourceServiceClient.java
@@ -5,7 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "api-gateway", configuration = FeignConfig.class)
+@FeignClient(name = "api-gateway", contextId = "resourceServiceClient", configuration = FeignConfig.class)
 public interface ResourceServiceClient {
 
     @GetMapping("/resource-service/resources/{id}")
