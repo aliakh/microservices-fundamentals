b9bfba13e1507e2fd68a44b08b0c16cf094345eb
Aliaksandr Liakh <Aliaksandr_Liakh1@epam.com>
2026-02-15 16:12:53 +0300

    Implement practical task for module 5 'Service Discovery'

--- resource-processor/src/main/java/com/example/resourceprocessor/service/ResourceServiceClient.java
+++ resource-processor/src/main/java/com/example/resourceprocessor/service/ResourceServiceClient.java
@@ -5,7 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "resource-service", configuration = FeignConfig.class)
+@FeignClient(name = "api-gateway", contextId = "resourceServiceClient", configuration = FeignConfig.class)
 public interface ResourceServiceClient {
 
     @GetMapping("/resources/{id}")
--- resource-processor/src/main/java/com/example/resourceprocessor/service/SongServiceClient.java
+++ resource-processor/src/main/java/com/example/resourceprocessor/service/SongServiceClient.java
@@ -7,7 +7,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-@FeignClient(name = "song-service", configuration = FeignConfig.class)
+@FeignClient(name = "api-gateway", contextId = "songServiceClient", configuration = FeignConfig.class)
 public interface SongServiceClient {
 
     @PostMapping("/songs")
