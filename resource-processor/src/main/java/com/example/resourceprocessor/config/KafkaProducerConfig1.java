7e89903953d49c1c12d2c71d8b3eb47c7e0c6167
Aliaksandr Liakh <Aliaksandr_Liakh1@epam.com>
2026-02-17 17:35:01 +0300

    k

---
+++ resource-processor/src/main/java/com/example/resourceprocessor/config/KafkaProducerConfig1.java
@@ -0,0 +1,51 @@
+package com.example.resourceprocessor.config;
+
+import com.example.resourceprocessor.dto.KafkaProperties;
+import org.apache.kafka.clients.admin.AdminClientConfig;
+import org.apache.kafka.clients.admin.NewTopic;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.LongSerializer;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.config.TopicBuilder;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaAdmin;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
+
+import java.util.HashMap;
+
+@EnableKafka
+@Configuration
+@EnableConfigurationProperties(value = KafkaProperties.class)
+public class KafkaProducerConfig1 {
+
+    @Bean
+    public KafkaAdmin kafkaAdmin(KafkaProperties kafkaProperties) {
+        var properties = new HashMap<String, Object>();
+        properties.put(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, kafkaProperties.bootstrapUrl());
+        return new KafkaAdmin(properties);
+    }
+
+    @Bean
+    public NewTopic newParsingResourcesTopic(KafkaProperties kafkaProperties) {
+        return TopicBuilder.name(kafkaProperties.parsingResourcesTopic()).build();
+    }
+
+    @Bean
+    public ProducerFactory<Long, String> producerFactory(KafkaProperties kafkaProperties) {
+        var properties = new HashMap<String, Object>();
+        properties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, kafkaProperties.bootstrapUrl());
+        properties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, LongSerializer.class);
+        properties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        return new DefaultKafkaProducerFactory<>(properties);
+    }
+
+    @Bean
+    public KafkaTemplate<Long, String> kafkaTemplate(ProducerFactory<Long, String> producerFactory) {
+        return new KafkaTemplate<>(producerFactory);
+    }
+}
--- resource-service/src/main/java/com/example/resourceservice/dto/KafkaProperties.java
+++ resource-service/src/main/java/com/example/resourceservice/dto/KafkaProperties.java
@@ -2,9 +2,12 @@ package com.example.resourceservice.dto;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
+import java.util.Map;
+
 @ConfigurationProperties(prefix = "kafka")
 public record KafkaProperties(
     String bootstrapUrl,
-    String parsingResourcesTopic
+    String parsingResourcesTopic,
+    Map<String, String> properties
 ) {
 }
