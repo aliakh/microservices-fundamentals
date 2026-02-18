588d264e7891bd5cf8bf38862402bdadf0af98cf
Aliaksandr Liakh <Aliaksandr_Liakh1@epam.com>
2026-02-18 11:12:10 +0300

    Fix

--- resource-service/src/main/java/com/example/resourceservice/service/StorageService.java
+++ resource-service/src/main/java/com/example/resourceservice/service/StorageService.java
@@ -3,8 +3,6 @@ package com.example.resourceservice.service;
 import com.example.resourceservice.dto.StorageDto;
 import com.example.resourceservice.dto.StorageType;
 import com.example.resourceservice.exception.StorageNotFoundException;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -13,24 +11,12 @@ import java.util.List;
 @Service
 public class StorageService {
 
-    private static final Logger logger = LoggerFactory.getLogger(StorageService.class);
-
     @Autowired
     private StorageServiceClient storageServiceClient;
 
-    //    @CircuitBreaker(name = "storageServiceClient", fallbackMethod = "getAllStoragesFallback")
     public List<StorageDto> getAllStorages() {
         return storageServiceClient.getAllStorages();
     }
-
-//    private List<StorageDto> getAllStoragesFallback(Exception e) {
-//        logger.warn("Failed to read storages, using a local fallback instead", e);
-//
-//        return List.of(
-//            new StorageDto(1L, StorageType.STAGING, "resources-staging", "/staging/"),
-//            new StorageDto(2L, StorageType.PERMANENT, "resources-permanent", "/permanent/")
-//        );
-//    }
 
     public StorageDto getStorageById(long storageId) {
         var storages = getAllStorages();
