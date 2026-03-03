5d1c625225da5cc8794c7548ee5d71fe717c8bb1
Aliaksandr Liakh <Aliaksandr_Liakh1@epam.com>
2026-03-03 22:46:09 +0300

    Implement practical task for module 7 'Monitoring'

--- resource-processor/src/main/java/com/example/resourceprocessor/service/ResourceFinalizingProducer.java
+++ resource-processor/src/main/java/com/example/resourceprocessor/service/ResourceFinalizingProducer.java
@@ -1,11 +1,17 @@
 package com.example.resourceprocessor.service;
 
 import com.example.resourceprocessor.dto.KafkaProperties;
+import com.example.resourceprocessor.tracing.TraceConstants;
+import com.example.resourceprocessor.tracing.TraceContext;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.header.internals.RecordHeader;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
+
+import java.nio.charset.StandardCharsets;
 
 @Service
 public class ResourceFinalizingProducer {
@@ -18,7 +24,15 @@ public class ResourceFinalizingProducer {
     private KafkaProperties kafkaProperties;
 
     public void finalizeResource(Long resourceId) {
-        kafkaTemplate.send(kafkaProperties.finalizingResourcesTopic(), resourceId, resourceId.toString())
+        var topic = kafkaProperties.finalizingResourcesTopic();
+        var key = resourceId;
+        var value = resourceId.toString();
+
+        var producerRecord = new ProducerRecord<>(topic, key, value);
+        var traceId = TraceContext.getTraceIdOrThrow();
+        producerRecord.headers().add(new RecordHeader(TraceConstants.TRACE_ID_HEADER, traceId.getBytes(StandardCharsets.UTF_8)));
+
+        kafkaTemplate.send(producerRecord)
             .whenComplete((result, throwable) -> {
                     if (throwable == null) {
                         logger.info("Resource finalizing message with key {} and value {} was published to topic {} at offset {}",
---
+++ storage-service/src/main/java/com/example/storageservice/tracing/TraceConstants.java
@@ -0,0 +1,7 @@
+package com.example.storageservice.tracing;
+
+public interface TraceConstants {
+
+    String TRACE_ID_HEADER = "X-Trace-Id";
+    String TRACE_ID_MDC = "xTraceId";
+}
