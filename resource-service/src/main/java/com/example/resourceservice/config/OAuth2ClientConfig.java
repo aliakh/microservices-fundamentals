731bf311c08f9b96e4ceabce91365a74dd29e072
Aliaksandr Liakh <Aliaksandr_Liakh1@epam.com>
2026-03-07 13:52:30 +0300

    Implement practical task for module 8

--- resource-service/src/main/java/com/example/resourceservice/config/OAuth2ClientConfig.java
+++ resource-service/src/main/java/com/example/resourceservice/config/OAuth2ClientConfig.java
@@ -2,9 +2,7 @@ package com.example.resourceservice.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.oauth2.client.AuthorizedClientServiceOAuth2AuthorizedClientManager;
-import org.springframework.security.oauth2.client.InMemoryOAuth2AuthorizedClientService;
-import org.springframework.security.oauth2.client.OAuth2AuthorizedClientManager;
+import org.springframework.security.oauth2.client.*;
 import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
 
 @Configuration
@@ -17,5 +15,19 @@ public class OAuth2ClientConfig {
                 clientRegistrationRepository,
                 new InMemoryOAuth2AuthorizedClientService(clientRegistrationRepository)
         );
+    }
+
+//    @Bean
+    OAuth2AuthorizedClientManager authorizedClientManager(
+        ClientRegistrationRepository clientRegistrationRepository,
+        OAuth2AuthorizedClientService clientService) {
+
+        var provider = OAuth2AuthorizedClientProviderBuilder.builder()
+            .clientCredentials()   // <-- IMPORTANT
+            .build();
+
+        var manager = new AuthorizedClientServiceOAuth2AuthorizedClientManager(clientRegistrationRepository, clientService);
+        manager.setAuthorizedClientProvider(provider);
+        return manager;
     }
 }
