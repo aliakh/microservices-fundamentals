7f6b37d8acc348f743958c7e477242f3e1684a81
Aliaksandr Liakh <Aliaksandr_Liakh1@epam.com>
2026-03-07 20:46:34 +0300

    Implement practical task for module 7 'Monitoring'

--- storage-service/src/main/java/com/example/storageservice/config/SecurityConfig.java
+++ storage-service/src/main/java/com/example/storageservice/config/SecurityConfig.java
@@ -1,11 +1,15 @@
 package com.example.storageservice.config;
 
+import com.example.storageservice.controller.StorageController;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AbstractAuthenticationToken;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
@@ -18,16 +22,19 @@ import java.util.Collection;
 @Configuration
 public class SecurityConfig {
 
+    private static final Logger logger = LoggerFactory.getLogger(SecurityConfig.class);
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(csrf -> csrf.disable())
+//                .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/actuator/**").permitAll()
-                        .requestMatchers(HttpMethod.GET, "/**").hasAnyRole("user", "admin")
-                        .requestMatchers(HttpMethod.POST, "/**").hasRole("admin")
-                        .requestMatchers(HttpMethod.DELETE, "/**").hasRole("admin")
-                        .anyRequest().authenticated()
+                    .requestMatchers("/actuator/**").permitAll()
+                    .requestMatchers(HttpMethod.GET, "/**").hasAnyRole("user", "admin")
+                    .requestMatchers(HttpMethod.POST, "/**").hasRole("admin")
+                    .requestMatchers(HttpMethod.DELETE, "/**").hasRole("admin")
+                    .anyRequest().authenticated()
                 )
                 .oauth2ResourceServer(oauth2 -> oauth2.jwt(jwt -> jwt.jwtAuthenticationConverter(jwtAuthenticationConverter())));
 
@@ -40,14 +47,14 @@ public class SecurityConfig {
             // Combine Keycloak roles + standard scopes
             Collection<GrantedAuthority> authorities = new ArrayList<>();
 
-            // Include Keycloak realm roles
+            // Include Keycloak realm/client roles
             authorities.addAll(new KeycloakRealmRoleConverter().convert(jwt));
 
-            // Include standard scopes
+            // Include OAuth2 scopes (e.g., SCOPE_openid, SCOPE_profile, etc.)
             JwtGrantedAuthoritiesConverter scopesConverter = new JwtGrantedAuthoritiesConverter();
             authorities.addAll(scopesConverter.convert(jwt));
 
-            System.out.println("Mapped authorities: " + authorities);
+            logger.info("Granted authorities: {}" , authorities);
 
             return authorities;
         });
