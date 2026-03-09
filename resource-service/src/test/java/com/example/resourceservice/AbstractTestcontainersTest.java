bc81de550641d4cd779a7e3a483b4e34826a2e6d
Aliaksandr Liakh <Aliaksandr_Liakh1@epam.com>
2026-03-09 20:59:20 +0300

    Fix tests

--- resource-service/src/test/java/com/example/resourceservice/AbstractTestcontainersTest.java
+++ resource-service/src/test/java/com/example/resourceservice/AbstractTestcontainersTest.java
@@ -1,41 +1,77 @@
 package com.example.resourceservice;
 
-import com.example.resourceservice.dto.KafkaProperties;
+import com.example.resourceservice.dto.StorageDto;
+import com.example.resourceservice.dto.StorageType;
+import com.example.resourceservice.entity.Resource;
+import com.example.resourceservice.service.ResourceParsingProducer;
+import com.example.resourceservice.service.ResourceService;
+import com.example.resourceservice.service.StorageService;
+import com.example.resourceservice.service.StorageServiceClient;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Primary;
 import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.util.ReflectionTestUtils;
 import org.testcontainers.containers.localstack.LocalStackContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
-import org.testcontainers.kafka.KafkaContainer;
 import org.testcontainers.utility.DockerImageName;
 import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
 import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
 
-import java.util.Map;
+import javax.annotation.PostConstruct;
+import java.util.List;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doAnswer;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 @Testcontainers
 @ContextConfiguration(classes = AbstractTestcontainersTest.TestConfig.class)
 public abstract class AbstractTestcontainersTest {
-
-    protected static final String PARSING_RESOURCES_TOPIC = "parsing_resources_topic";
-    protected static final String FINALIZING_RESOURCES_TOPIC = "finalizing_resources_topic";
 
     @Container
     protected static final LocalStackContainer LOCAL_STACK =
         new LocalStackContainer(DockerImageName.parse("localstack/localstack"))
             .withServices(LocalStackContainer.Service.S3);
 
-    @Container
-    protected static final KafkaContainer KAFKA =
-        new KafkaContainer(DockerImageName.parse("apache/kafka"));
-
     static {
         LOCAL_STACK.start();
-        KAFKA.start();
+    }
+
+    @Autowired
+    private StorageService storageService;
+    @Autowired
+    private ResourceService resourceService;
+
+    @PostConstruct
+    void mockStorageServiceClient() {
+        var storageServiceClient = mock(StorageServiceClient.class);
+
+        var storages = List.of(
+            new StorageDto(1L, StorageType.STAGING, "resources-staging", "/staging/"),
+            new StorageDto(2L, StorageType.PERMANENT, "resources-permanent", "/permanent/")
+        );
+        when(storageServiceClient.getAllStorages()).thenReturn(storages);
+
+        ReflectionTestUtils.setField(storageService, "storageServiceClient", storageServiceClient);
+    }
+
+    @PostConstruct
+    void mockResourceParsingProducer() {
+        var resourceParsingProducer = mock(ResourceParsingProducer.class);
+
+        doAnswer(invocation -> {
+            Resource resource = invocation.getArgument(0);
+            resourceService.moveResourceToPermanentStorage(resource.getId());
+            return null;
+        }).when(resourceParsingProducer).parseResource(any());
+
+        ReflectionTestUtils.setField(resourceService, "resourceParsingProducer", resourceParsingProducer);
     }
 
     @TestConfiguration
@@ -53,17 +89,6 @@ public abstract class AbstractTestcontainersTest {
                 )
                 .region(Region.of(LOCAL_STACK.getRegion()))
                 .build();
-        }
-
-        @Bean
-        @Primary
-        public KafkaProperties kafkaProperties() {
-            return new KafkaProperties(
-                KAFKA.getBootstrapServers(),
-                PARSING_RESOURCES_TOPIC,
-                FINALIZING_RESOURCES_TOPIC,
-                Map.of()
-            );
         }
     }
 }
