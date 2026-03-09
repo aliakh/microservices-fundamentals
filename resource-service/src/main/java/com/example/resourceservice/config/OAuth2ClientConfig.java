b2a7a41016915422a21fee389cb78463731785d9
Aliaksandr Liakh <Aliaksandr_Liakh1@epam.com>
2026-03-09 11:30:13 +0300

    Implement practical task for module 8 'Security'

---
+++ resource-service/src/main/java/com/example/resourceservice/config/OAuth2ClientConfig.java
@@ -0,0 +1,85 @@
+package com.example.resourceservice.config;
+
+import feign.RequestInterceptor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.oauth2.client.AuthorizedClientServiceOAuth2AuthorizedClientManager;
+import org.springframework.security.oauth2.client.InMemoryOAuth2AuthorizedClientService;
+import org.springframework.security.oauth2.client.OAuth2AuthorizeRequest;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientManager;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientProviderBuilder;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.oauth2.core.OAuth2AuthorizationException;
+import org.springframework.security.oauth2.core.OAuth2Error;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class OAuth2ClientConfig {
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+            .csrf(AbstractHttpConfigurer::disable)
+            .authorizeHttpRequests(amrmr -> amrmr
+                .anyRequest().permitAll()
+            );
+        return http.build();
+    }
+
+    @Bean
+    OAuth2AuthorizedClientService oAuth2AuthorizedClientService(ClientRegistrationRepository clientRegistrationRepository) {
+        return new InMemoryOAuth2AuthorizedClientService(clientRegistrationRepository);
+    }
+
+    @Bean
+    OAuth2AuthorizedClientManager authorizedClientManager(ClientRegistrationRepository clientRegistrationRepository,
+                                                          OAuth2AuthorizedClientService authorizedClientService) {
+        var authorizedClientProvider = OAuth2AuthorizedClientProviderBuilder.builder()
+            .clientCredentials()
+            .build();
+
+        var authorizedClientManager = new AuthorizedClientServiceOAuth2AuthorizedClientManager(
+            clientRegistrationRepository,
+            authorizedClientService
+        );
+
+        authorizedClientManager.setAuthorizedClientProvider(authorizedClientProvider);
+        return authorizedClientManager;
+    }
+
+    @Bean
+    RequestInterceptor outh2FeignRequestInterceptor(OAuth2AuthorizedClientManager authorizedClientManager) {
+        return template -> {
+            var principal = new AnonymousAuthenticationToken(
+                "resource-service-key",
+                "resource-service-principal",
+                AuthorityUtils.createAuthorityList("ROLE_ANONYMOUS")
+            );
+
+            var authorizeRequest = OAuth2AuthorizeRequest
+                .withClientRegistrationId("storage-service-client")
+                .principal(principal)
+                .build();
+
+            var authorizedClient = authorizedClientManager.authorize(authorizeRequest);
+            if (authorizedClient == null || authorizedClient.getAccessToken() == null) {
+                throw new OAuth2AuthorizationException(
+                    new OAuth2Error(
+                        "authorization_failed",
+                        "Failed to acquire access token for client registration 'storage-service-client'",
+                        null
+                    )
+                );
+            }
+
+            template.header(HttpHeaders.AUTHORIZATION, "Bearer " + authorizedClient.getAccessToken().getTokenValue());
+            template.header(HttpHeaders.ACCEPT, "application/json");
+        };
+    }
+}
