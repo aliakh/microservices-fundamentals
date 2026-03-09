7a4553a663ca5665f0d8af8b1609b9506b147f0b
Aliaksandr Liakh <Aliaksandr_Liakh1@epam.com>
2026-03-09 19:48:34 +0300

    Fix tests

--- resource-processor/src/test/java/com/example/resourceprocessor/config/KafkaMockConfig.java
+++ resource-processor/src/test/java/com/example/resourceprocessor/config/KafkaMockConfig.java
@@ -3,7 +3,6 @@ package com.example.resourceprocessor.config;
 import com.example.resourceprocessor.dto.KafkaProperties;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Primary;
 import org.springframework.kafka.core.KafkaTemplate;
 
 import static org.mockito.Mockito.mock;
@@ -12,13 +11,11 @@ import static org.mockito.Mockito.mock;
 public class KafkaMockConfig {
 
     @Bean
-    @Primary
     public KafkaTemplate<Long, String> kafkaTemplateMock() {
         return mock(KafkaTemplate.class);
     }
 
     @Bean
-    @Primary
     public KafkaProperties kafkaProperties() {
         return mock(KafkaProperties.class);
     }
